431d26fe315a8db1f59d27f03de708519fe6e2bf
freaklucas <lucas.oliveira.freak@gmail.com>
2022-05-30 17:20:14 -0300

    rep limpo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,3 @@
 <template>
-  <div class="justify-center flex bg-yellow-300 items-center h-screen">
-    <div class="text-4xl">Hello 👋🏼</div>
-  </div>
+  <router-view />
 </template>
-<script>
-export default {
-  name: "App",
-};
-</script>
