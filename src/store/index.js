3e34a201fd3ef440e87232b81aaceefa813590ee
freaklucas <lucas.oliveira.freak@gmail.com>
2022-05-30 23:34:25 -0300

    recuperando name api ok

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,33 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+
+import axios from "axios";
 
 export default createStore({
   state: {
+    products: [],
   },
+
   mutations: {
+    GET_PRODUCTS: function (state, payload) {
+      state.products = payload;
+    },
   },
   actions: {
+    getProducts: async function ({ commit }) {
+      try {
+        let dataUrl = `https://jsonplaceholder.typicode.com/users`;
+        let response = await axios.get(dataUrl);
+
+        commit("GET_PRODUCTS", response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    },
   },
-  modules: {
-  }
-})
+  getters: {
+    listProducts(state) {
+      return state.products.length;
+    },
+  },
+  modules: {},
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,32 @@
 <template>
   <div class="justify-center flex bg-yellow-300 items-center h-screen">
     <div class="text-4xl">Hello 👋🏼</div>
+
+    <div class="flex flex-col justify-center items-center">
+      <h2
+        class="font-semibold text-2xl"
+        v-for="product in products"
+        :key="product.id"
+      >
+        {{ product.name }}
+      </h2>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: "Home",
   components: {},
+
+  created: function () {
+    this.$store.dispatch("getProducts");
+  },
+
+  computed: {
+    ...mapState(["products"]),
+  },
 };
 </script>
