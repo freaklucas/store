07c487278c47f66af69fa41e4d1e18b6e4040bd9
freaklucas <lucas.oliveira.freak@gmail.com>
2022-05-31 23:38:47 -0300

    busca renderizando img ok

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,16 @@
   <div class="justify-center flex bg-yellow-300 items-center h-screen">
 
     <div class="flex flex-col justify-center items-center">
-      <h2
+      <div
         class="font-semibold text-2xl"
         v-for="product in products"
         :key="product.id"
       >
-        {{ product.png }}
-      </h2>
+        <img 
+          :src="product.image" 
+          alt="imagem dos produtos"
+        />        
+      </div>
     </div>
   </div>
 </template>
