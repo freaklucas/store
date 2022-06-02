62ccd83adb28a2f35d17fb7e0f75681aca539e64
freaklucas <lucas.oliveira.freak@gmail.com>
2022-06-01 21:56:19 -0300

    add menu lateral

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,109 @@
 <template>
-  <div class="justify-center flex items-center h-screen">
+  <div class="justify-center flex items-center">
     <section class="bg-white dark:bg-gray-900">
       <div class="container px-6 py-8 mx-auto">
         <div class="lg:flex lg:-mx-2">
+          <div class="space-y-3 lg:w-1/5 lg:px-2 lg:space-y-4">
+            <a
+              href="#"
+              class="
+                block
+                font-medium
+                text-gray-500
+                dark:text-gray-300
+                hover:underline
+              "
+              >Jackets & Coats</a
+            >
+            <a
+              href="#"
+              class="
+                block
+                font-medium
+                text-gray-500
+                dark:text-gray-300
+                hover:underline
+              "
+              >Hoodies</a
+            >
+            <a
+              href="#"
+              class="
+                block
+                font-medium
+                text-blue-600
+                dark:text-blue-500
+                hover:underline
+              "
+              >T-shirts & Vests</a
+            >
+            <a
+              href="#"
+              class="
+                block
+                font-medium
+                text-gray-500
+                dark:text-gray-300
+                hover:underline
+              "
+              >Shirts</a
+            >
+            <a
+              href="#"
+              class="
+                block
+                font-medium
+                text-gray-500
+                dark:text-gray-300
+                hover:underline
+              "
+              >Blazers & Suits</a
+            >
+            <a
+              href="#"
+              class="
+                block
+                font-medium
+                text-gray-500
+                dark:text-gray-300
+                hover:underline
+              "
+              >Jeans</a
+            >
+            <a
+              href="#"
+              class="
+                block
+                font-medium
+                text-gray-500
+                dark:text-gray-300
+                hover:underline
+              "
+              >Trousers</a
+            >
+            <a
+              href="#"
+              class="
+                block
+                font-medium
+                text-gray-500
+                dark:text-gray-300
+                hover:underline
+              "
+              >Shorts</a
+            >
+            <a
+              href="#"
+              class="
+                block
+                font-medium
+                text-gray-500
+                dark:text-gray-300
+                hover:underline
+              "
+              >Underwear</a
+            >
+          </div>
           <div class="mt-6 lg:mt-0 lg:px-2 lg:w-4/5">
             <div
               class="
